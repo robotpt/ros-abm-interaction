6d91a70120264a5e7e7e3a3a952a1c2fca236b0f
Audrow <audrow.nash@gmail.com>
2020-02-08 19:10:12 +0000

    Make ROS client for cordial

---
+++ scripts/abm_cordial_client.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3.6
+
+import rospy
+
+from interaction_engine.messager.message import Message
+from pickled_database import PickledDatabase
+
+from abm_grant_interaction import state_db, param_db
+from abm_grant_interaction.abm_interaction import AbmInteraction
+
+import rospy
+from cordial_gui.srv import Ask, AskRequest
+
+from interaction_engine.interfaces.interface import Interface
+from interaction_engine.messager.message import Message
+
+
+class AbmCordialClient(Interface):
+
+    def __init__(
+            self,
+            service_topic,
+            pickled_database=None,
+            is_create_db_key_if_not_exist=True,
+            node_name='cordial_client',
+    ):
+        super().__init__(
+            self.ask,
+            pickled_database=pickled_database,
+            is_create_db_key_if_not_exist=is_create_db_key_if_not_exist
+        )
+
+        rospy.init_node(node_name, anonymous=True)
+        self._service_topic = service_topic
+        self._client = rospy.ServiceProxy(self._service_topic, Ask)
+
+    def ask(self, message):
+
+        if type(message) is not Message:
+            raise ValueError("Must input message class")
+
+        rospy.wait_for_service(self._service_topic)
+
+        try:
+            ask_request = AskRequest()
+            ask_request.display.type = message.message_type
+            ask_request.display.content = message.content
+            ask_request.display.buttons = message.options
+
+            resp = self._client(ask_request)
+            return resp.result
+
+        except rospy.ServiceException as e:
+            print("Service call failed: {}".format(e))
+
+
+if __name__ == '__main__':
+
+    interface = AbmCordialClient(
+        service_topic='cordial/say_and_ask_on_gui',
+        pickled_database=state_db,
+    )
+    abm_interaction = AbmInteraction(interface=interface)
+
+    while not rospy.is_shutdown():
+        abm_interaction.run_scheduler_once()
+        rospy.sleep(1.)
+        rospy.logdebug("Ran loop")
+
+
