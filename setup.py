7d4a94eb9d2b5b61cb1272bf03202451587d695c
Audrow <audrow.nash@gmail.com>
2020-02-09 16:35:08 +0000

    Update path to pickled database + remove fitbitreader

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ package_dirs = {
     'abm_grant_interaction': os.path.join('src', 'abm-grant-interaction', 'abm_grant_interaction'),
     'abm_fitbit_client': os.path.join('src', 'abm_fitbit_client'),
     'fitbit_client': os.path.join('src', 'fitbit-client', 'fitbit_client'),
-    'fitbit_reader': os.path.join('src', 'fitbit-reader', 'fitbit_reader'),
     'interaction_engine': os.path.join('src', 'interaction-engine', 'interaction_engine'),
-    'pickle_database': os.path.join('src', 'pickle-database', 'pickle_database'),
+    'pickled_database': os.path.join('src', 'pickle-database', 'pickled_database'),
     'robotpt_common_utils': os.path.join('src', 'robotpt-common-utils', 'robotpt_common_utils'),
 }
 
