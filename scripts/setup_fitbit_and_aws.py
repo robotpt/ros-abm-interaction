87430b50ae76d6c68377cad173de0ba19bf32a00
Audrow <audrow.nash@gmail.com>
2020-02-11 08:44:43 -0800

    Create script to setup Fitbit and AWS credentials

---
+++ scripts/setup_fitbit_and_aws.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3.6
+
+import os
+import rospy
+import subprocess
+import json
+
+from fitbit_client import FitbitClient
+
+
+# Setup fitbit
+credentials_file_path=rospy.get_param(
+    'abm/fitbit/credentials/path',
+    default='/root/state/fitbit_credentials.yaml'
+)
+redirect_url=rospy.get_param(
+    'abm/fitbit/redirect_url',
+    default="http://localhost",
+)
+FitbitClient(
+    credentials_file_path=credentials_file_path,
+    redirect_url=redirect_url,
+)
+
+# Setup AWS
+os.system("aws configure")
+
+out = subprocess.check_output(["aws", "sts", "get-caller-identity"])
+out = json.loads(out)
+print(out, type(out))
